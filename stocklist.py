bcb6c10d217084be892129d28aa8fb57fe7b63aa
Magnus Peterson Munoz <magnuspetersonmunoz@gmail.com>
2021-08-26 08:03:18 -0600

    Add generator to stocklist

--- stocklist.py
+++ stocklist.py
@@ -1,25 +1,44 @@
+# from pandas._libs.tslibs.timestamps import Tick
 import yfinance as yf
 import pandas as pd
 import csv
 from yfinance import shared
 
+
 file = open("tickers.csv", "r")
 csv_reader = csv.reader(file)
 
+def gen_csv():
+    for row in csv_reader:
+        yield ''.join(row)
+
+
+
+
+
+load = gen_csv()
 list_from_csv = []
-for row in csv_reader:
-    list_from_csv.append(''.join(row))
+for i in load:
+    list_from_csv.append(i) 
+    tick = yf.Tickers(i)
+    a= tick.tickers.keys()
+    try:
+        ticker_df = tick.history(period='1d')
+        print(f"{tick.tickers.keys()} does has downloaded")
+    except:
+        print(f"{tick.tickers.keys()} does not exist")
+        list_from_csv.remove(list(a)[0])
+        continue
+    print(i)
 
-tick = yf.Tickers(list_from_csv)
-
-ticker_df = tick.history(period='1d')
 
 keys_to_rm = list(shared._ERRORS.keys())
 keys_to_rm.append('0')
 
-a = 0
-while a < len(keys_to_rm)-1:
-    for c in range(len(keys_to_rm)-1):
+
+def gen_stock():
+    a = 0
+    while a < len(keys_to_rm)-1:
         list_from_csv.remove(keys_to_rm[a])
         a+=1
 
